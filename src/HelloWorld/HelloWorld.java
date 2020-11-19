9aeee114de5045819639f991040caa99ee2ed0b5
Amy Nguyen <amei.nguyennct@gmail,com>
2020-11-18 22:27:58 -0500

    update a simple Java program from zybook

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -1,10 +1,26 @@
 package HelloWorld;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world, this is Amy Nguyen!");
+		Scanner scnr = new Scanner(System.in);
+	    double x = scnr.nextDouble();
+	    double y = scnr.nextDouble();
+	    double z = scnr.nextDouble();
+	    /* Type your code here. */
+	    double xPowerz = Math.pow(x,z);
+	    double xPowerYToZ = Math.pow(x, Math.pow(y,z));
+	    double absoluteY = Math.abs(y);
+	    double squareRootXYPowerZ = Math.sqrt(Math.pow((x*y),z));
+	    System.out.println(xPowerz + " " + xPowerYToZ + " " + absoluteY + " " + squareRootXYPowerZ);
+	      
+	      
 	}
 
 }
+
+
