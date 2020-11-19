72295877a89676f9e72063434adbb786ecbf4e9f
Amy Nguyen <amei.nguyennct@gmail,com>
2020-11-19 00:06:45 -0500

    update

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Hello world, this is Amy Nguyen!");
 		Scanner scnr = new Scanner(System.in);
 	    double x = scnr.nextDouble();
